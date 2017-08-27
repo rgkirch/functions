4de5d8a1e7d1336e2a134ca82d10acc8edb05b41
Richie Kirchofer <rgkirch@gmail.com>
2017-08-27 12:58:49 -0400

    hetero vector

--- main.cpp
+++ main.cpp
@@ -1,58 +1,30 @@
-#include <iostream>
 #include <vector>
-#include <string>
-#include <memory>
-#include <algorithm>
-#include <cassert>
+#include <iostream>
 
 using namespace std;
 
-template<typename T>
-void draw(const T &x, ostream &out, size_t position) { out << string(position, ' ') << x << endl; }
-
 struct Object {
-    template<typename T>
-    Object(const T &x) : object_(new model<T>(x)) {}
-    friend void draw(const Object &o, ostream &out, size_t position) { o.object_->then(out, position); }
-    struct concept_t {
-        virtual ~concept_t() = default;
-        virtual concept_t *copy_() = 0;
-        virtual void then(ostream &, size_t) const = 0;
+    template <typename T>
+    Object(T t) : model(new Model<T>(t)) {}
+    struct Concept {
+        virtual void print() =0;
     };
-
-    template<typename T>
-    struct model : concept_t {
-        model(const T &x) : data_(x) {}
-        concept_t *copy_() { return new model(*this); }
-        void then(ostream &out, size_t position) const { draw(data_, out, position); }
-        T data_;
+    template <typename T>
+    struct Model : Concept {
+        Model(T t) : data(t) {}
+        void print() {
+            cout << data << endl;
+        }
+        T data;
     };
-
-    unique_ptr<concept_t> object_;
+    Concept *model;
 };
-
-struct Indirection {
-    Indirection(Object x) : object_m(make_shared<implementation_t>(std::move(x))) {}
-    struct implementation_t {
-        explicit implementation_t(Object x) : value_m(std::move(x)) {}
-        Object value_m;
-    };
-    shared_ptr<implementation_t> object_m;
-};
-
-void draw(const vector<Indirection> &x, ostream &out, size_t position) {
-    for (auto &e : x) draw(e.object_m.get()->value_m, out, position + 2);
-}
-
-struct my_class_t {
-    /* ... */
-};
-
-void draw(const my_class_t &, ostream &out, size_t position) { out << string(position, ' ') << "my_class_t" << endl; }
 
 int main() {
-    vector<Indirection> h;
-    h.emplace_back(0);
-    h.emplace_back(string("Hello!"));
-    draw(h, cout, 0);
+    vector<Object> vect;
+    vect.push_back(0);
+    vect.push_back("hello");
+    vect[0].model->print();
+    vect[1].model->print();
 }
+
