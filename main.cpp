e9b298f0d6c986427a4df68815b08443c2e4494e
Richie Kirchofer <rgkirch@gmail.com>
2017-08-27 18:56:17 -0400

    now have a heterogeneous vector of things. thank you friendly people on stack overflow

--- main.cpp
+++ main.cpp
@@ -5,68 +5,87 @@
 
 using namespace std;
 
+//template <typename T>
+//void print(T&&);      // <- so that Object::Model knows what you want.
+
+void print(const std::string &s) {
+    std::cout << s << '\n';
+}
+
+void print(const int i) {
+    std::cout << i << '\n';
+}
+
 struct Object {
-    template <typename T>
+
+    template<typename T>
     Object(T t) : model(new Model<T>(t)) {
         cout << "construct object from T" << endl;
     }
-    friend void print(string str) {
-        cout << str << endl;
-    }
-    friend void print(int i) {
-        cout << i << endl;
-    }
+
     struct Concept {
+        virtual ~Concept() = default;
+
         virtual void print() =0;
     };
-    template <typename T>
+
+    template<typename T>
     struct Model : Concept {
-        Model() {};
-        void print() override {
-            cout << "haha" << endl;
+        Model(T t) : data(t) {}
+
+        void print() {
+            ::print(data);
         }
+
+        T data;
     };
-    struct Model<string> : Concept {
-        Model(string t) : data(t) {}
-        void print() override {
-            cout << data << endl;
-        }
-        string data;
-    };
-    struct Model<int> : Concept {
-        Model(int t) : data(t) {}
-        void print() override {
-            cout << data << endl;
-        }
-        int data;
-    };
+
     Concept *model;
+
 };
+
 
 struct For {
     For() {
         cout << "default construct For" << endl;
     }
+
     For(Object o) {
         cout << "construct For from object and push back" << endl;
         objects.push_back(o);
     }
+
     For operator()(Object o) {
         cout << "push back object" << endl;
         objects.push_back(o);
         return *this;
     }
+
     void print() {
-        for(auto o : objects) {
+        for (auto o : objects) {
             o.model->print();
         }
     };
+
     vector<Object> objects;
 };
 
 int main() {
     auto heterogeneous = For(1)(string("hello"));
-    function<void(int)> f = [](int i)->void{ cout << i << endl; };
     heterogeneous.print();
 }
 
+
+//    function<function<void(std::string)>(int)> f = [&](int i) -> function<void(std::string)> {
+//        function<void(std::string)> g = [&](string str) -> void {
+//            cout << i << ' ' << str << endl;
+//        };
+//        return g;
+//    };
+//    heterogeneous.yield(f);
+
+//    void yield(function<function<void(std::string)>(int)> f) {
+//        for (auto ints : objects[0]) {
+//            function<void(std::string)> g = o.apply(f);
+//        }
+//    }
