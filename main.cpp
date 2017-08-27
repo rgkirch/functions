a0d1222443430d8563b53aa6a7b44cae381f537b
Richie Kirchofer <rgkirch@gmail.com>
2017-08-27 15:38:34 -0400

    why no work? get error. same as before. ask internet?

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <vector>
+#include <string>
 #include <functional>
 #include <iostream>
 
@@ -9,20 +10,21 @@ struct Object {
     Object(T t) : model(new Model<T>(t)) {
         cout << "construct object from T" << endl;
     }
+    friend void print(string str) {
+        cout << str << endl;
+    }
+    friend void print(int i) {
+        cout << i << endl;
+    }
     struct Concept {
         virtual void print() =0;
-//        template <typename T>
-//        virtual void apply(function<void(T)> f) =0;
     };
     template <typename T>
     struct Model : Concept {
         Model(T t) : data(t) {}
         void print() {
-            cout << data << endl;
+            print(data);
         }
-//        void apply(function<void(T)> f) {
-//            f(data);
-//        }
         T data;
     };
     Concept *model;
@@ -41,18 +43,17 @@ struct For {
         objects.push_back(o);
         return *this;
     }
-    template <typename T>
-    void apply(function<void(T)> f) {
+    void print() {
         for(auto o : objects) {
-//            o.model->apply(f);
+            o.model->print();
         }
     };
     vector<Object> objects;
 };
 
 int main() {
-    auto heterogenious = For(1)("hello");
+    auto heterogeneous = For(1)(string("hello"));
     function<void(int)> f = [](int i)->void{ cout << i << endl; };
-    heterogenious.apply(f);
+    heterogeneous.print();
 }
 
