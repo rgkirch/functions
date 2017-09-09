f99353955ae282efeb38316bd3675140351eb164
Richie Kirchofer <rgkirch@gmail.com>
2017-09-09 18:44:40 -0400

    kinda working for comprehension using dynamic cast

--- for.h
+++ for.h
@@ -6,9 +6,39 @@
 
 using namespace std;
 
+class object_t {
+public:
+    template<typename T>
+    object_t(const T &x) : object_(new model<T>(x)) {}
+    object_t(const object_t &x) : object_(x.object_->copy_()) { cout << "copy" << endl; }
+    object_t(object_t &&x) = default;
+    object_t &operator=(object_t x) {
+        object_ = move(x.object_);
+        return *this;
+    }
+
+    friend void draw(const object_t &x, ostream &out, size_t position) { x.object_->draw_(out, position); }
+
+private:
+    struct concept_t {
+        virtual ~concept_t() = default;
+        virtual concept_t *copy_() = 0;
+        virtual void draw_(ostream &, size_t) const = 0;
+    };
+
+    template<typename T>
+    struct model : concept_t {
+        model(const T &x) : data_(x) {}
+        concept_t *copy_() { return new model(*this); }
+        void draw_(ostream &out, size_t position) const { draw(data_, out, position); }
+        T data_;
+    };
+
+    unique_ptr <concept_t> object_;
+};
+
 template <typename T>
 struct For {
-//    typedef For<Args...> super;
     For(T t) : t(t) {
         cout << __PRETTY_FUNCTION__ << endl;
     }
@@ -23,8 +53,7 @@ struct For {
             f->then(fp);
         }
     }
-    T t;
-    unique_ptr<For> f;
+    shared_ptr<object_t> object_m;
 };
 
 
--- main.cpp
+++ main.cpp
@@ -1,112 +1,125 @@
 #include <vector>
+#include <memory>
 #include <string>
 #include <functional>
 #include <iostream>
 
-using namespace std;
 
-//template <typename T>
-//void print(T&&);      // <- so that Object::Model knows what you want.
+// Default print; stream to cout:
+template <typename T>
+void print(T const& t){ std::cout << t <<"\n"; }
 
-void print(const std::string &s) {
-    std::cout << s << '\n';
+// Functional-style loops:
+struct Object;
+using Body=std::function<void(Object)>;
+using Loop=std::function<void(Body)>;
+
+// default foreach: return a loop that just visits yourself:
+template <typename T>
+Loop foreach(T const& t) {
+    return [t](Body body){ body(t); };
 }
 
-void print(const int i) {
-    std::cout << i << '\n';
+// custom printing:
+inline void print(std::string const& str) {
+    std::cout << "\"" << str  <<"\"" << std::endl;
 }
+inline void print(int i) {
+    std::cout << "Integer: "<< i << std::endl;
+}
+// such custom prints can go here, or in the namespace of the type.
 
+// Objects are actually smart pointers to type-erased data:
 struct Object {
-
-    template<typename T>
-    Object(T t) : model(new Model<T>(t)) {
-        cout << "construct object from T" << endl;
+    template <typename T>
+    Object(T t) : model(std::make_shared<Model<T>>(std::forward<T>(t))) {
+        //  std::cout << "construct object from T" << std::endl;
     }
 
+    friend void print(Object o){
+        o.model->do_print();
+    }
+    friend Loop foreach(Object o){
+        return o.model->do_foreach();
+    }
     struct Concept {
-        virtual ~Concept() = default;
-
-        virtual void apply() =0;
+        virtual void do_print() =0;
+        virtual Loop do_foreach() = 0;
+        // no dtor, as shared ptr handles that
     };
 
-    template<typename T>
-    struct Model : Concept {
-        Model(T t) : data(t) {}
-
-        void apply() {
+    template <typename T>
+    struct Model final : Concept {
+        Model(T t) : data(std::forward<T>(t)) {}
+        void do_print() override {
             print(data);
         }
-
+        Loop do_foreach( ) override {
+            return foreach(data);
+        }
         T data;
     };
-
-    Concept *model;
-
+    std::shared_ptr<Concept> model;
 };
 
 
-//template<>
-//struct Object::Model<int> : Concept {
-//    Model(int t) : data(t) {}
-//
-//    void print() {
-//        ::print(data);
-//    }
-//
-//    int data;
-//};
-//
-//template<>
-//struct Object::Model<string> : Concept {
-//    Model(string t) : data(t) {}
-//
-//    void print() {
-//        ::print(data);
-//    }
-//
-//    string data;
-//};
-
 struct For {
-    For() {
-        cout << "default construct For" << endl;
+    For() {}
+    template<class...Ts>
+    For(Ts const&...ts):
+            objects({ts...})
+    {}
+    // given an Object o, return that object's contents
+    // in a For.
+    static For box(Object o) {
+        For tmp;
+        foreach(o)([&tmp](Object o){ tmp+=o; });
+        return tmp;
     }
-
-    For(Object o) {
-        cout << "construct For from object and push back" << endl;
-        objects.push_back(o);
+    For(For const&)=default;
+    For& operator+=(Object o){ objects.push_back(o); return *this; }
+    For operator+(Object o) const { For tmp=*this; tmp+= o; return tmp; }
+    For operator*(For rhs)const{
+        For tmp;
+        for( auto o1 : objects )
+            for( auto o2 : rhs.objects )
+            {
+                tmp += box(o1) + o2;
+            }
+        return tmp;
     }
-
-    For operator()(Object o) {
-        cout << "push back object" << endl;
-        objects.push_back(o);
+    For& operator*=(For rhs){
+        *this = *this*rhs;
+    }
+    template<class...Ts>
+    For operator()(Ts const&... ts) {
+        *this *= For(ts...);
         return *this;
     }
+    friend Loop foreach(For const& f){
+        return [f](Body body){
+            for(auto o : f.objects) {
+                body(o);
+            }
+        };
+    }
 
-    void print() {
-        for (auto o : objects) {
-            o.model->apply();
+    friend void print(For f) {
+        std::cout << "{";
+        for(auto o : f.objects) {
+            print(o);
         }
+        std::cout << "}\n";
     };
-
-    vector<Object> objects;
+    std::vector<Object> objects;
 };
 
 int main() {
-    auto heterogeneous = For(1)(string("hello"));
-    heterogeneous.print();
+    auto a = For(std::string("hello"),42)(1,-1);
+    auto b = a;
+    b('a','b','c');
+    std::cout <<"a:\n";
+    print(a);
+    std::cout <<"b:\n";
+    print(b);
 }
-
-//    function<function<void(std::string)>(int)> f = [&](int i) -> function<void(std::string)> {
-//        function<void(std::string)> g = [&](string str) -> void {
-//            cout << i << ' ' << str << endl;
-//        };
-//        return g;
-//    };
-//    heterogeneous.yield(f);
-
-//    void yield(function<function<void(std::string)>(int)> f) {
-//        for (auto ints : objects[0]) {
-//            function<void(std::string)> g = o.apply(f);
-//        }
-//    }
