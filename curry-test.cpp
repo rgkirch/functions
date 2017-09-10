281a70229077b8942c316ffd56bebed18da96e95
Richie Kirchofer <rgkirch@gmail.com>
2017-09-10 12:17:03 -0400

    made a curry function

--- curry-test.cpp
+++ curry-test.cpp
@@ -4,7 +4,7 @@
 using namespace std;
 
 int main() {
-    auto sum = [=](auto a, auto b, auto c){ return a + b + c; };
+    function<int(int, int, int)> sum = [=](auto a, auto b, auto c){ return a + b + c; };
     auto curried = curry(sum);
     cout << curried(1)(2)(3) << endl;
 
--- curry.hpp
+++ curry.hpp
@@ -1,8 +1,13 @@
 #include <functional>
 using namespace std;
+
+template <typename R, typename T>
+auto curry(const function<R(T)> f) {
+    return f;
+}
 template <typename R, typename T, typename... Args>
 auto curry(const function<R(T, Args...)> f) {
     return [=](T t) {
-        return [=](Args... args){ return f(t, args...); };
+        return curry((function<R(Args...)>)[=](Args... args){ return f(t, args...); });
     };
 }
