782e1e0da010579015b5e79771a548ae6bd821ad
richie <rgkirch@gmail.com>
2017-09-22 14:42:09 -0400

    match is working

--- match.hpp
+++ match.hpp
@@ -2,13 +2,17 @@
 
 using namespace std;
 
+template <typename T>
 struct Match {
-    template <typename T>
-    Match(T t) {}
+  Match(T* t) : t(t) {}
     template <typename Ret, typename Arg>
-    Match& operator()(function<Ret(Arg)> f) {
-        cout << "hi from case \n";
-        cout << __PRETTY_FUNCTION__ << '\n';
-        return *this;
+    Match& operator()(function<Ret(Arg*)> f) {
+      auto cast = dynamic_cast<Arg*>(t);
+      if(cast) {
+        f(cast);
+      }
+      // cout << __PRETTY_FUNCTION__ << '\n';
+      return *this;
     }
+  T* t;
 };
